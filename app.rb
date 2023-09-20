4f9d23d32128ddc7249fd887a46670a2fb78141a
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-20 17:10:05 +0000

    finished paper

--- app.rb
+++ app.rb
@@ -9,3 +9,13 @@ get("/rock") do
   @opp_hand = rand(1..3)
   erb(:rock)
 end
+
+get("/scissors") do
+  @opp_hand = rand(1..3)
+  erb(:scissors)
+end
+
+get("/paper") do
+  @opp_hand = rand(1..3)
+  erb(:paper)
+end
